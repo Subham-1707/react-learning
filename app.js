f2ba03c36585fad3fc0d39bd29a3c09b24af6a43
Subham-1707 <subhamk2002@gmail.com>
2026-05-25 16:08:08 +0530

    lecture 4 props vitual dom

--- app.js
+++ app.js
@@ -1,92 +1,83 @@
-/* 
-PARCEL - A BUNDLER:
-----------------------------
-  It is a feature provided by bundlers like Webpack and Parcel.
-   1. HMR - Hot Module Replacement
-   2. File watchers algorithm - C++. It watches for changes in the source code files and triggers a reload when a change is detected.
-   3. BUNDLER
-   4. MINIFY
-   5. CLEANING OUR CODE
-   6. DEV AND PRODUCTION BUILD
-   7. SUPER FAST BUILD TIME
-   8. IMAGE OPTIMIZATION
-   9. CACHE WHILE DEVELOPMENT
-   10. COMPATIBILITY WITH OLDER BROWSERS
-   11. COMPRESSION
-   12. MANAGE PORT NO
-   13. ZERO CONFIGURATION
-   14. CREATE A SERVER 
-
-   TRANSITIVE DEPENDENCIES - If A depends on B and B depends on C,
-    then A has a transitive dependency on C.
-    In other words, A indirectly depends on C through B.
-
-    browserlist - It is a configuration file used in web development to
-     specify the target browsers for a project. It helps tools like Babel and 
-     Autoprefixer to determine which JavaScript features and CSS prefixes 
-     to include in the final output, ensuring compatibility with the specified browsers.
-*/
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// React element - because it is returning a jsx element
-// writing JSX code - JavaScript XML
-const container = (
-    <div
-        id="container"
-        style={{
-            backgroundColor: 'blue',
-            padding: '10px',
-            borderRadius: '10px'  
-          }}
-    >
-        <h1
-            className="main-title"
-            key="heading1"
-            style={{
-                color: 'black',
-                backgroundColor: 'green',
-                fontFamily: 'Arial'
-            }}
-        >
-            Namaste, React! from H1
-        </h1>
-        <h2
-            className="main-title"
-            key="heading2"
-            style={{
-                color: 'black',
-                backgroundColor: 'red',
-                fontFamily: 'Arial'
-            }}
-        >
-            Namaste, React! from H2
-        </h2>
-    </div>
+import {restaurantList} from './lib/restaurantApi'
 
-)
-
-const Title = () => ( <h3>Namaste, React! from Title</h3> )
+const Title = () => ( 
+   <a href = '/'>
+    <img 
+     className='logo'
+     alt='logo'
+     src='https://yt3.ggpht.com/ytc/AKedOLSpK3T_2RxkMYb-pk9oENQB0NvYpeOdXRgQe8i5=s800-c-k-c0x00ffffff-no-rj'/>
+    </a>
+ )
 // React component - It is a function that returns a React element
 // component composition - It is the process of combining multiple components to create a new component. It is a way to reuse code and create complex UIs from simple components.
-const HeadingComponent =  () => {
+const Header =  () => {
     return (
-        <div className='outer'>  
-        {container} 
-              <h1>Namaste, React! from HeadingComponent</h1>
-                <HeadingComponent2 />
-                {Title()} 
+        <div className='header'>  
+            <Title />
+             <div className='nav-item'>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+             </div>
+               
         </div>
      
     )
 }
 
-const HeadingComponent2 =  () => (
-  <h2>Namaste, greeting from subham</h2>
-)
-   
+const imagePath = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/'
+
+const RestaurantCard = ({name, cuisines, avgRating, cloudinaryImageId, platformCharge}) => {
+    return (
+        <div className='restaurant-card'>       
+            <img src = {imagePath + cloudinaryImageId}  alt = 'restaurant-image' />
+            <h4>{cuisines.join(', ')}</h4>
+            <p>{ name}</p>
+            <p>Rating: { avgRating || 3.5}</p>
+            <p>Platform charge: {platformCharge || 0}</p>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className='restaurant-list'> 
+          {
+            restaurantList.map( restaurant => {
+                return <RestaurantCard  key={restaurant.data.id} {...restaurant.data}  platformCharge = '5' />
+            })
+          }
+          
+        </div>
+    )
+}
+
+const Footer = () => {
+    return (
+        <div className='footer'> 
+          <h2>Footer Content</h2>
+        </div>
+    )
+}
+
+
+
+const AppLayout = () => {
+   return (
+     <>  
+        <Header />
+        <Body />
+        <Footer />
+     </>
+   )
+}
+ 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-// root.render(container)
-root.render(<HeadingComponent />)
+root.render(<AppLayout />)
